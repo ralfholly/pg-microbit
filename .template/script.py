76ba886c359aaac52321bbce96cb6425353af990
Ralf Holly <ralf.holly@approxion.com>
2018-03-09 20:58:14 +0100

    Display Image.HAPPY instead of non-existent Image.Happy

--- .template/script.py
+++ .template/script.py
@@ -2,7 +2,7 @@ import microbit as mbit
 
 def main():
     # TODO: Your code here...
-    mbit.display.show(mbit.Image.Happy)
+    mbit.display.show(mbit.Image.HAPPY)
 
 if __name__ == '__main__':
     main()
